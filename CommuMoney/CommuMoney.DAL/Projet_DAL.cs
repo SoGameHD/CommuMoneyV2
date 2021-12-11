e4e858f7c647f584dad6f01f0b3f9b06914e799b
Snowballe <62845660+Snowballe@users.noreply.github.com>
2021-12-11 22:24:48 +0100

    ajout DAL Projet

---
+++ CommuMoney/CommuMoney.DAL/Projet_DAL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommuMoney.DAL
+{
+    public class Projet_DAL
+    {
+        public int ID { get; set; }
+        public string NOM{ get; set; }
+        public int ID_PERSONNE { get; set; }
+        public float TOTAL_MONTANT { get; set; }
+        public float MOYENNE { get; set; }
+        public DateTime? Updated_at { get; set; }
+        public DateTime? Created_at { get; set; }
+
+        public Projet_DAL(int id, string nom,int id_personne,float total_montant, float moyenne, DateTime? created_at)
+        {
+            ID = id;
+            NOM = nom;
+            ID_PERSONNE = id_personne;
+            TOTAL_MONTANT = total_montant;
+            MOYENNE = moyenne;
+            Created_at = created_at;
+
+        }
+
+    }
+}
