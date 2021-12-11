9c7ebb1d77550c0490934e6ce37ce3c5ffff29e1
SoGameHD <erwann.leducq@epsi.fr>
2021-12-11 23:59:27 +0100

    Couche Metier V1

--- CommuMoney/CommuMoney.METIER/Metier/Depenses_METIER.cs
+++ CommuMoney/CommuMoney.METIER/Metier/Depenses_METIER.cs
@@ -15,7 +15,7 @@ namespace CommuMoney.METIER.Metier
         public int ID_Projet { get; set; }
         public float Montant { get; set; }
         public DateTime? Created_At { get; set; }
-        public DateTime? Updated_At { get; set; };
+        public DateTime? Updated_At { get; set; }
 
         public Depenses_METIER(int id_personne, int id_projet, float montant, DateTime? created_at, DateTime? updated_at) => (ID_Personne, ID_Projet, Montant, Created_At, Created_At) = (id_personne, id_projet, montant, created_at, updated_at);
         public Depenses_METIER(int id, int id_personne, int id_projet, float montant, DateTime? created_at, DateTime? updated_at) => (ID, ID_Personne, ID_Projet, Montant, Created_At, Created_At) = (id, id_personne, id_projet, montant, created_at, updated_at);
@@ -44,7 +44,7 @@ namespace CommuMoney.METIER.Metier
         public void Delete()
         {
             Depenses_DAL depense = new Depenses_DAL(ID, ID_Projet, ID_Personne, Montant, Created_At, Updated_At);
-            var depotDepense = new DepensesDepot_DAL;
+            var depotDepense = new DepensesDepot_DAL();
             depotDepense.Delete(depense);
         }
         #endregion
